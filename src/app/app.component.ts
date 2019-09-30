e24c9e1c1d893512897b3f1a026f929949b42546
sreelekshmils77 <sreelekshmi.ls@experionglobal.com>
2019-09-30 14:26:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gitsearch';
+  tagName = `NO DATA TO DISPLAY`;
+  dataAvailable: boolean = false;
+  newData: boolean = false;
+  details;
+  profile = '';
+
+  constructor(private httpClient: HttpClient, private toastr: ToastrService) { }
+
+  search(user) {
+    this.newData = true;
+    if (localStorage.getItem(user) != null) {
+      this.dataAvailable = true;
+      this.newData = false;
+      this.details = JSON.parse(localStorage.getItem(user));
+      this.tagName = user;
+    } else {
+      // tslint:disable-next-line: max-line-length
+      this.httpClient.get(`https://api.github.com/users/` + user).subscribe(
+        (res: any) => {
+          this.newData = false;
+          this.dataAvailable = true;
+          this.details = res;
+          this.tagName = user;
+          this.profile = JSON.stringify(this.details);
+          localStorage.setItem(user, this.profile);
+        },
+        () => {
+          this.tagName = `NO DATA TO DISPLAY`;
+          this.dataAvailable = false;
+          this.toastr.warning('Invalid User');
+        }
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
